b831ec1e73065857d2095520666e9616d889b237
huang-jing <137173735+dream123321@users.noreply.github.com>
2025-12-14 14:18:37 +0800

    Add files via upload

---
+++ cli/create_lammps_model.py
@@ -0,0 +1,22 @@
+import sys
+
+import torch
+from e3nn.util import jit
+
+from ptagnn.calculators import LAMMPS_MACE
+
+
+def main():
+    assert len(sys.argv) == 2, f"Usage: {sys.argv[0]} model_path"
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model_path = sys.argv[1]  # takes model name as command-line input
+    model = torch.load(model_path,map_location=device)
+#    model = model.double().to("cpu")
+    print(model)
+    lammps_model = LAMMPS_MACE(model)
+    lammps_model_compiled = jit.compile(lammps_model)
+    lammps_model_compiled.save(model_path + "-lammps.pt")
+
+
+if __name__ == "__main__":
+    main()
---
+++ modules/__init__.py
@@ -0,0 +1,123 @@
+from typing import Callable, Dict, Optional, Type
+
+import torch
+
+from .blocks import (
+    AgnosticNonlinearInteractionBlock,
+    AgnosticResidualNonlinearInteractionBlock,
+    AtomicEnergiesBlock,
+    EquivariantProductBasisBlock,
+    InteractionBlock,
+    LinearDipoleReadoutBlock,
+    LinearNodeEmbeddingBlock,
+    LinearReadoutBlock,
+    NonLinearDipoleReadoutBlock,
+    NonLinearReadoutBlock,
+    RadialEmbeddingBlock,
+    RealAgnosticAttResidualInteractionBlock,
+    RealAgnosticInteractionBlock,
+    RealAgnosticResidualInteractionBlock,
+    ResidualElementDependentInteractionBlock,
+    ScaleShiftBlock,
+)
+from .myblocks import (
+    AtomicEnergiesBlock,
+    EquivariantProductBasisBlock,
+    InteractionBlock,
+    LinearNodeEmbeddingBlock,
+    LinearReadoutBlock,
+    NonLinearReadoutBlock,
+    RadialEmbeddingBlock,
+    Residual_InteractionBlock,
+    ScaleShiftBlock,
+)
+
+from .loss import (
+    DipoleSingleLoss,
+    UniversalLoss,
+    WeightedEnergyForcesDipoleLoss,
+    WeightedEnergyForcesL1L2Loss,
+    WeightedEnergyForcesLoss,
+    WeightedEnergyForcesStressLoss,
+    WeightedEnergyForcesVirialsLoss,
+    WeightedForcesLoss,
+    WeightedHuberEnergyForcesStressLoss,
+)
+from .models import (
+    MACE,
+    AtomicDipolesMACE,
+    BOTNet,
+    EnergyDipolesMACE,
+    ScaleShiftBOTNet,
+    ScaleShiftMACE,
+)
+from .mymodels import (
+    FAMS_DNN
+)
+from .radial import BesselBasis, GaussianBasis, PolynomialCutoff, ChebyshevBasis
+from .symmetric_contraction import SymmetricContraction
+from .utils import (
+    compute_avg_num_neighbors,
+    compute_fixed_charge_dipole,
+    compute_mean_rms_energy_forces,
+    compute_mean_std_atomic_inter_energy,
+    compute_rms_dipoles,
+)
+
+interaction_classes: Dict[str, Type[InteractionBlock]] = {
+    "AgnosticNonlinearInteractionBlock": AgnosticNonlinearInteractionBlock,
+    "ResidualElementDependentInteractionBlock": ResidualElementDependentInteractionBlock,
+    "AgnosticResidualNonlinearInteractionBlock": AgnosticResidualNonlinearInteractionBlock,
+    "RealAgnosticResidualInteractionBlock": RealAgnosticResidualInteractionBlock,
+    "RealAgnosticAttResidualInteractionBlock": RealAgnosticAttResidualInteractionBlock,
+    "RealAgnosticInteractionBlock": RealAgnosticInteractionBlock,
+    "Residual_InteractionBlock": Residual_InteractionBlock,
+}
+
+scaling_classes: Dict[str, Callable] = {
+    "std_scaling": compute_mean_std_atomic_inter_energy,
+    "rms_forces_scaling": compute_mean_rms_energy_forces,
+    "rms_dipoles_scaling": compute_rms_dipoles,
+}
+
+gate_dict: Dict[str, Optional[Callable]] = {
+    "abs": torch.abs,
+    "tanh": torch.tanh,
+    "silu": torch.nn.functional.silu,
+    "None": None,
+}
+
+__all__ = [
+    "AtomicEnergiesBlock",
+    "RadialEmbeddingBlock",
+    "LinearNodeEmbeddingBlock",
+    "LinearReadoutBlock",
+    "EquivariantProductBasisBlock",
+    "ScaleShiftBlock",
+    "LinearDipoleReadoutBlock",
+    "NonLinearDipoleReadoutBlock",
+    "InteractionBlock",
+    "NonLinearReadoutBlock",
+    "PolynomialCutoff",
+    "BesselBasis",
+    "ChebyshevBasis",
+    "GaussianBasis",
+    "MACE",
+    "ScaleShiftMACE",
+    "BOTNet",
+    "ScaleShiftBOTNet",
+    "AtomicDipolesMACE",
+    "EnergyDipolesMACE",
+    "WeightedEnergyForcesLoss",
+    "WeightedForcesLoss",
+    "WeightedEnergyForcesVirialsLoss",
+    "WeightedEnergyForcesStressLoss",
+    "DipoleSingleLoss",
+    "WeightedEnergyForcesDipoleLoss",
+    "WeightedHuberEnergyForcesStressLoss",
+    "SymmetricContraction",
+    "interaction_classes",
+    "compute_mean_std_atomic_inter_energy",
+    "compute_avg_num_neighbors",
+    "compute_fixed_charge_dipole",
+]
